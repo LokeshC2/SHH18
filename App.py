dfcbf8f7df4254e57935a3e7dca3c510cda66182
LokeshC2 <47241840+LokeshC2@users.noreply.github.com>
2022-11-11 23:42:16 +0530

    fixes

--- App.py
+++ App.py
@@ -9,7 +9,7 @@ def do_some_work (x):
         print(r)
     return "File recieved"
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static', static_folder='static', template_folder='templates')
 
 @app.route('/api', methods=['POST'])
 def upload():
